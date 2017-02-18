d77868bf5788e9acfa28fb5343532181f3112741
CNMSwarmTeam <jschlindwein@cnm.edu>
2017-02-18 12:42:45 -0700

    update
    post class update of searchController.h

--- CNM-SRWG 2017/mobility/src/SearchController.h
+++ CNM-SRWG 2017/mobility/src/SearchController.h
@@ -8,11 +8,22 @@
  * This class implements the search control algorithm for the rovers. The code
  * here should be modified and enhanced to improve search performance.
  */
-class SearchController {
+
+
+
+class SearchController
+{
 
   public:
 
     SearchController();
+
+    //sets centerSeen bool
+    void setCenterSeen(bool answer);
+
+    void setCenterLocation(geometry_msgs::Pose2D newLocation);
+
+    void cnmSetRotations(int num);
 
     // performs search pattern
     geometry_msgs::Pose2D search(geometry_msgs::Pose2D currentLocation);
@@ -21,8 +32,13 @@ class SearchController {
     geometry_msgs::Pose2D continueInterruptedSearch(geometry_msgs::Pose2D currentLocation, geometry_msgs::Pose2D oldGoalLocation);
 
   private:
-
+    //VARIABLES
+    //--------------------------------------
     random_numbers::RandomNumberGenerator* rng;
+
+    int cnmNumRotations;
+
+
 };
 
 #endif /* SEARCH_CONTROLLER */
